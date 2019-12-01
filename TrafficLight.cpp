9039a78656fd105d25dd9938010552f782249ece
Rachel Morris <rachel@Iggly.local>
2019-12-01 18:24:35 -0500

    TrafficLight updates

--- TrafficLight.cpp
+++ TrafficLight.cpp
@@ -6,14 +6,56 @@
 class TrafficLight
 {
 
-  TrafficLight::TrafficLight(Color c, int green, int yellow, int red, int time)
+  TrafficLight::TrafficLight(Color col) //, int green, int yellow, int red, int time)
   {
-    lightColor = c;
-    timeGreen = green;
-    timeYellow = yellow;
-    timeRed = red;
-    timeTilChange = time; //maybe do not need
+    lightColor = col;
+    //timeGreen = green;
+    //timeYellow = yellow;
+    //timeRed = red;
+    //timeTilChange = time;
   }
+
+  Color TrafficLight::getColor()
+  {
+    return lightColor;
+  }
+
+  Color TrafficLight::setColor(Color c)
+  {
+    Color old = lightColor;
+    lightColor = c;
+    return old;
+  }
+
+  /*void TrafficLight::increment()
+  {
+    timeTilChange--;
+    if(timeTilChange == 0)
+    {
+      if(lightColor == GREEN)
+      {
+        lightColor = YELLOW;
+        timeTilChange = timeYellow;
+      }
+
+      if(lightColor == YELLOW)
+      {
+        lightColor = RED;
+        timeTilChange = timeRed;
+      }
+
+      if(lightColor == RED)
+      {
+        lightColor = GREEN;
+        timeTilChange = timeGreen;
+      }
+    }
+  } */
+
+  /*int TrafficLight::getTimeTilChange()
+  {
+    return timeTilChange;
+  } */
 
 }
 
--- TrafficLight.h
+++ TrafficLight.h
@@ -9,16 +9,16 @@ enum Color {GREEN, YELLOW, RED};
 
 private:
 	Color lightColor;
-  int timeGreen;
-  int timeYellow;
-  int timeRed;
-  int timeTilChange;
+  //int timeGreen;
+  //int timeYellow;
+  //int timeRed;
+  //int timeTilChange; //maybe do not need
 
 public:
   Color getColor();
-  Color setColor();
-  void  increment(); //will need either set color of increment, depends on whether light keeps track of time or game keeps track of time
-  int   getTimeTilChange();
+  Color setColor(Color c);
+  //void  increment(); //will need either set color of increment, depends on whether light keeps track of time or game keeps track of time
+  //int   getTimeTilChange();
 
 };
 
