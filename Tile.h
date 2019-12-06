48e86967416f2d467a096681ed0c748ba19adadc
Rachel Morris <rachel@Iggly.local>
2019-12-06 13:32:11 -0500

    updates to tile.h

--- Tile.h
+++ Tile.h
@@ -1,7 +1,6 @@
 #ifndef __TILE_H__
 #define __TILE_H__
 
-#include "Vehicle.h"
 
 class Tile
 {
@@ -9,10 +8,17 @@ class Tile
 private:
 	bool isEmpty;
 	Tile* next;
-public: 
-   Tile();
-   bool isOccupied();
-   void setOccupied();
+public:
+	Tile();
+	bool isOccupied();
+	void setOccupied();
+
+public:
+	Tile();
+	Tile(Vehicle* v1);
+	~Tile();
+	Vehicle* getVehicle();
+
 };
 
 #endif
