e7e2a4b2c632ff178a888184b45d1bed60e8764a
Jing Dong <jing.dong@richmond.edu>
2019-12-07 01:31:04 -0500

    added method to Road.cpp

--- Road.h
+++ Road.h
@@ -4,15 +4,20 @@
 #include "IntersectionTile.h"
 #include "Tile.h"
 #include <vector>
+#include "Vehicle.h"
 using namespace std;
 
 class Road
 {
 	private:
 		vector<Tile> road;
+    vector<vehicle> vehicles;
 	public:
 		Road();
 		Road(int num, Tile start,Intersection a, Intersection b);
+    void addVehicle(Vehicle a){
+        vehicles.push_back(a);
+    }
 };
 
 #endif
