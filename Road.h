b2708625bc6c6944a47dec6d3318c2645da9d2f1
Jack Ditto <jackditto7@gmail.com>
2019-12-08 15:18:49 -0500

    Fix formatting

--- Road.h
+++ Road.h
@@ -10,12 +10,12 @@ using namespace std;
 class Road
 {
 	private:
-		vector<Tile> road;
-    vector<vehicle> vehicles;
+	   vector<Tile> road;
+      vector<vehicle> vehicles;
 	public:
 		Road();
 		Road(int num, Tile start,Intersection a, Intersection b);
-    void addVehicle(Vehicle a){
+      void addVehicle(Vehicle a){
         vehicles.push_back(a);
     }
 };
